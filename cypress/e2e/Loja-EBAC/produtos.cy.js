38a247c27cef13506ed7582f2f6bafa48ba220f1
Gabriel <gabrielhtognato@gmail.com>
2024-06-07 22:47:56 -0300

    listas

---
+++ cypress/e2e/Loja-EBAC/produtos.cy.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade: produtos', () => {
+    
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+    
+    it('deve selecionar um produto da lista', () => {
+        cy.get('.product-block')
+            //.first()
+            //.last()
+            //.eq(2)
+            .contains('Apollo Running Short')
+            .click()
+            cy.get('#tab-description > :nth-child(2)').should('exist')
+        
+    });
+});
