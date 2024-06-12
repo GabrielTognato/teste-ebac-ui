c45bdfc1c946189f23351dbca46621d88d0c61a8
Gabriel <gabrielhtognato@gmail.com>
2024-06-12 11:05:00 -0300

    arquivo de dados

--- cypress/e2e/Loja-EBAC/login.cy.js
+++ cypress/e2e/Loja-EBAC/login.cy.js
@@ -1,22 +1,32 @@
 /// <reference types="cypress"/>
+const perfil= require('../../fixtures/perfil.json')
 
 describe('funcionalidade : login', () => {
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta/')
     });
     afterEach(() => {
         cy.screenshot()
     });
 
- it('Deve fazer login com sucesso', () => {
-    
-    cy.get('#username').type ('gabriel@teste.com.br')
-    cy.get('#password').type ('teste@123')
-    cy.get('.woocommerce-form > .button').click()
-    cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, gabriel-8834')
- })
+ it ('deve fazer login com sucesso - Usando massa de dados', () => {
+     cy.get('#username').type (perfil.usuario)
+     cy.get('#password').type (perfil.senha)
+     cy.get('.woocommerce-form > .button').click()
+     cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, gabriel-8834')
+     });
 
+ it.only ('deve fazer login com sucesso - fixture', () => {
+     cy.fixture('perfil').then( dados => {
+     cy.get('#username').type (dados.usuario)
+     cy.get('#password').type (dados.senha , {log: false})
+     cy.get('.woocommerce-form > .button').click()
+     cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, gabriel-8834')
+      })
+});    
+   
+ 
  it('deve exibir uma mensagem de erro ao inserir usuario invalido', () => {
     
     cy.get('#username').type ('gabriell@teste.com.br')
@@ -35,4 +45,5 @@ describe('funcionalidade : login', () => {
 
  });
 
+ 
 })
--- cypress/e2e/Loja-EBAC/produtos.cy.js
+++ cypress/e2e/Loja-EBAC/produtos.cy.js
@@ -3,7 +3,7 @@
 describe('Funcionalidade: produtos', () => {
     
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+        cy.visit('produtos')
     });
     
     it('deve selecionar um produto da lista', () => {
